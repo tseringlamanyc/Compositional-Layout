0677340c97014f54f1e984c91d49ecc8bf19f30c
Tsering Lama <admin@Tserings-iMac.local>
2020-10-26 21:57:16 -0400

    added data source

--- Compositional-Layout/GridViewController.swift
+++ Compositional-Layout/GridViewController.swift
@@ -9,22 +9,59 @@ import UIKit
 
 class GridViewController: UIViewController {
     
-    @IBOutlet weak var collectionView: UICollectionView!
+    // Setup enum to hold sections
+    enum Section {
+        case main
+    }
+    
+    @IBOutlet weak var collectionView: UICollectionView! // default is Flow
+    
+    // declare our data source, diffable data source
+    // both section identifier needs to be Hashable
+    private var dataSource: UICollectionViewDiffableDataSource<Section, Int>!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureCollectionView()
     }
     
-    private func createLayout() {
-        // 1) Create and configure the item
-        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.25), heightDimension: .fractionalHeight(1.0))
+    private func configureCollectionView() {
+       // collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createLayout())  *programmatic*
+        
+        // change the collectionview's layout *storyboard*
+        collectionView.collectionViewLayout = createLayout()
+        collectionView.backgroundColor = .systemBackground
         
     }
-
-
-    private func configureCollectionView() {
+    
+    private func createLayout() -> UICollectionViewLayout {
+        // 1) Create and configure the item
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.25), heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
         
+        //2) Create and configure group
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalWidth(0.25))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        
+        //3) Configure section
+        let section = NSCollectionLayoutSection(group: group)
+        
+        //4) Configure layout
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        
+        return layout
+    }
+    
+    // configure Data Source
+    private func confiugreDataSource() {
+        // 1)
+        dataSource = UICollectionViewDiffableDataSource<Section, Int>(collectionView: collectionView, cellProvider: { (collectionView, indexpath, item) -> UICollectionViewCell? in
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "labelCell", for: indexpath) as? LabelCell else {
+                fatalError("could not deque")
+            }
+            return cell 
+        })
     }
     
 }
